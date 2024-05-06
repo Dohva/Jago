f98b989039f9043e0d1be677c326af18397c95d0
dohva <rallethekid@gmail.com>
2024-05-06 14:30:18 +0200

    Commit funky NSwag library

---
+++ src/ApiClientGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using Jago.Generator;
+
+System.Console.WriteLine(">>> Begin");
+
+var httpClient = new HttpClient();
+
+var client = new Client(httpClient);
+
+var response = await client.GetStatusAsync();
+
+System.Console.WriteLine($">>> {response.Status}");
+
+System.Console.WriteLine(">>> End");
